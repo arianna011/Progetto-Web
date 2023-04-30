fa815626ac2c5c3b1f56989f64d78841599d1b25
Federicasinisi <sinisifede@gmail.com>
2023-04-30 13:51:00 +0200

    2-commit-fede

--- Artisti.php
+++ Artisti.php
@@ -59,6 +59,11 @@
             flex-flow: column wrap;
           }
 
+          .badge {
+            margin: 1px;
+          }
+          
+
     </style>
 
 </head>
@@ -66,33 +71,49 @@
  <header>
   <?php include 'navbar.php'; ?>
   </header>
-
+  <div class="row " id="cover"  style="background: url('/images/pres1.jpg') no-repeat; background-size: cover; height:100px;">
+          <h1 class="text-center text-white p-4" >Artisti</h2>
+  </div>
   <div class="container-fed">
-    <form class="row g-2" method="post" id="search-form">
-      <div class="col-auto">
-        <input type="search" class="form-control" id="inputsearch" placeholder="Cerca" name="search">
-      </div>
-      <div class="col-auto">
-        <button type="submit" id='search' class="btn btn-secondary mb-3">
-        <i class="bi bi-search" style="color: #640062" ></i>
-        </button>
-      </div>
-    </form>
+
     <div class= "row align-items-start" id="row2">
         <div class="col-3">
-          <div class="form-check">
+        <form class="row g-2" method="post" id="search-form">
+        <div class="col-auto">
+          <input type="search" class="form-control" id="inputsearch" placeholder="Cerca" name="search">
+        </div>
+        <div class="col-auto">
+          <button type="submit" id='search' class="btn btn-secondary mb-3">
+          <i class="bi bi-search" style="color: #640062" ></i>
+          </button>
+        </div>
+        </form>
+          <select class="form-select" id="citta">
+          <option value="0" selected>Seleziona una città</option>
+          <?php
+            $query = "SELECT * FROM citta";
+            $result = pg_query($dbconn, $query) or die('Query failed: ' . pg_last_error());
+            $citta = pg_fetch_all($result);
+            foreach ($citta as $c) {
+              ?> 
+              <option value="<?= $c['id_citta']; ?>"> <?= $c['nome_citta']; ?> </option>
+              <?php
+            }
+          ?>
+          </select>
+          <div class="form-check m-2">
             <input class="form-check-input" type="radio" name="ordine" id="radio1"  value="recenti" checked>
             <label class="form-check-label" for="radio1"> Più recenti </label>
           </div>
-          <div class="form-check">
+          <div class="form-check m-2">
             <input class="form-check-input" type="radio" name="ordine" id="radio2"  value="migliori">
             <label class="form-chekc-label" for="radio2"> Migliori valutazioni</label>
           </div>
-          <div class="form-check">
-            <input class="form-check-input" type="radio" name="ordine" id="radio3"  value="economici">
+          <div class="form-check m-2">
+            <input class="form-check-input" type="radio" name="ordine" id="radio3"  value="prezzo">
             <label class="form-chekc-label" for="radio3"> Più economici </label>
           </div>
-            <div class="accordion" id="accordionFlushExample">
+          <div class="accordion" id="accordionFlushExample">
               <div class="accordion-item">
                 <h2 class="accordion-header">
                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
@@ -109,7 +130,7 @@
                           foreach ($generi as $genere) {
                             ?> 
                             <div class="form-check form-check-inline" >
-                            <input class="form-check-input" type="checkbox" name="genere" id="<?= $genere['id_strumento']; ?>" value="<?= $genere['nome_strumento']; ?>" />
+                            <input class="form-check-input" type="checkbox" name="strumento" id="<?= $genere['id_strumento']; ?>" value="<?= $genere['nome_strumento']; ?>" />
                             <label class="form-check-label" for="<?= $genere['id_strumento']; ?>"> <?= $genere['nome_strumento']; ?> </label>
                             </div>
                             <?php
@@ -122,41 +143,69 @@
               </div>
            </div>
 
-    <div class="accordion" id="accordionFlushExample">
-      <div class="accordion-item">
-        <h2 class="accordion-header">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseOne">
-            Filtra per genere
-          </button>
-        </h2>
-        <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-          <div class="accordion-body">
-            <div class="section-fed" >
-              <?php
-                  $query = "SELECT * FROM genere_musicale";
-                  $result = pg_query($dbconn, $query) or die('Query failed: ' . pg_last_error());
-                  $generi = pg_fetch_all($result);
-                  foreach ($generi as $genere) {
-                    ?> 
-                    <div class="form-check form-check-inline" >
-                    <input class="form-check-input" type="checkbox" name="genere" id="<?= $genere['id_genere']; ?>" value="<?= $genere['nome_genere']; ?>" />
-                    <label class="form-check-label" for="<?= $genere['id_genere']; ?>"> <?= $genere['nome_genere']; ?> </label>
+            <div class="accordion" id="accordionFlushExample">
+              <div class="accordion-item">
+                <h2 class="accordion-header">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseOne">
+                    Filtra per genere
+                  </button>
+                </h2>
+                <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                  <div class="accordion-body">
+                    <div class="section-fed" >
+                      <?php
+                          $query = "SELECT * FROM genere_musicale";
+                          $result = pg_query($dbconn, $query) or die('Query failed: ' . pg_last_error());
+                          $generi = pg_fetch_all($result);
+                          foreach ($generi as $genere) {
+                            ?> 
+                            <div class="form-check form-check-inline" >
+                            <input class="form-check-input" type="checkbox" name="genere" id="<?= $genere['id_genere']; ?>" value="<?= $genere['nome_genere']; ?>" />
+                            <label class="form-check-label" for="<?= $genere['id_genere']; ?>"> <?= $genere['nome_genere']; ?> </label>
+                            </div>
+                            <?php
+                          }
+                      
+                      ?>
                     </div>
-                    <?php
-                  }
-              
-              ?>
+                  </div>  
+                </div>
+              </div>
             </div>
-          </div>  
-        </div>
-      </div>
+            <div class="accordion" id="accordionFlushExample">
+              <div class="accordion-item">
+                <h2 class="accordion-header">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseOne">
+                    Filtra per servizi offerti
+                  </button>
+                </h2>
+                <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                  <div class="accordion-body">
+                    <div class="section-fed" >
+                      <?php
+                          $query = "SELECT * FROM servizio_musicale";
+                          $result = pg_query($dbconn, $query) or die('Query failed: ' . pg_last_error());
+                          $servizi = pg_fetch_all($result);
+                          foreach ($servizi as $servizio) {
+                            ?> 
+                            <div class="form-check form-check-inline" >
+                            <input class="form-check-input" type="checkbox" name="servizio" id="<?= $servizio['id_servizio'] ?>" value="<?= $servizio['nome_servizio']; ?>" />
+                            <label class="form-check-label" for="<?= $servizio['id_servizio']; ?>"> <?= $servizio['nome_servizio']; ?> </label>
+                            </div>
+                            <?php
+                          }
+                      
+                      ?>
+                    </div>
+                  </div>  
+                </div>
+              </div>
+            </div>
+
+         </div>
+        <div class="col-9" id="pull_data"></div>
     </div>
   </div>
-  <div class="col-9" id="pull_data">
-
-  </div>
-</div>
-</div>
 
 
 <footer>
@@ -165,11 +214,19 @@
 
 <script>
 
-function fetch_data(page,search){
+let ordine ;
+let strumenti = new Array();
+let generi = new Array();
+let search;
+let citta;
+let servizi = new Array();
+
+
+function fetch_data(page){
   $.ajax({
     url:"fetch_artisti.php",
     method:"POST",
-    data:{page:page, search:search},
+    data:{page:page, search:search, ordine:ordine, strumenti:strumenti, generi:generi, citta:citta, servizi:servizi},
     success:function(data){
       $('#pull_data').html(data);
     }
@@ -180,14 +237,76 @@ fetch_data();
 
 $(document).on('click', '.page-item', function(){
   let page = $(this).attr("id");
-  let search = $('#inputsearch').val();
-  fetch_data(page, search);
+  //let search = $('#inputsearch').val();
+  fetch_data(page);
 });
 
 $("#search-form").submit(function(event) {
-  let search = $('#inputsearch').val();
-  fetch_data(1, search);
+  search = $('#inputsearch').val();
+  fetch_data(1);
   event.preventDefault();
+});
+
+$(".form-select").change(function() {
+  if($(this).val() == "Seleziona una città"){
+    citta = NULL;
+  }else {
+  citta = $(this).val();
+  }
+  fetch_data(1);
+});
+
+/*
+$(document).on('click', '[name="ordine"]', function(){
+  let ordine = $(this).val();
+  fetch_data(1, search, ordine);
+});
+*/
+document.getElementsByName("ordine").forEach(function(element) {
+  element.addEventListener('click', function() {
+    ordine = $(this).val();
+    //let search = $('#inputsearch').val();
+    fetch_data(1);
+  });
+});
+
+document.getElementsByName("strumento").forEach(function(element) {
+  element.addEventListener('change', function() {
+    //let search = $('#inputsearch').val();
+    if(this.checked){
+    strumenti.push($(this).val());
+    fetch_data(1);
+    }
+    else{
+      strumenti.splice(strumenti.indexOf($(this).val()), 1);
+      fetch_data(1);
+    }
+  });
+});
+
+
+document.getElementsByName("genere").forEach(function(element) {
+  element.addEventListener('change', function() {
+    if(this.checked) {
+      generi.push($(this).val());
+      fetch_data(1);
+    } else {
+      generi.splice(generi.indexOf($(this).val()), 1);
+      fetch_data(1);
+    }
+  });
+});
+
+document.getElementsByName("servizio").forEach(function(element) {
+  element.addEventListener('change', function() {
+    if(this.checked) {
+      servizi.push($(this).val());
+      fetch_data(1);
+    } else {
+      servizi.splice(servizi.indexOf($(this).val()), 1);
+      fetch_data(1);
+    }
+  });
 });
 
 </script>
--- fetch_artisti.php
+++ fetch_artisti.php
@@ -1,8 +1,11 @@
 <?php
 include 'connection.php'; 
-$limit = 10;
+$limit = 5;
 $page = 0;
 $display = "";
+$strum = "";
+$gen = "";
+$serv = "";
 
 if (isset($_POST['page'])) {
     $page = $_POST['page'];
@@ -16,6 +19,45 @@ if (isset($_POST['search'])) {
     $search = "";
 }
 
+if (isset($_POST['ordine'])) {
+    if($_POST['ordine'] == "prezzo"){
+        $ordine = "min_prezzo";
+    } else if($_POST['ordine'] == "migliori"){
+        $ordine = "valutazione_media DESC";
+    }else {
+        $ordine = "id_artista DESC";
+    }
+}else{
+    $ordine = "id_artista DESC";
+}
+
+if(isset($_POST['strumenti'])) {
+    $strumenti = $_POST['strumenti'];
+    $strum = '';
+    foreach($strumenti as $str) {
+        $strum .= " '".pg_escape_string($dbconn, $str )."' = any(strumenti_musicali) OR ";
+    }
+    $strum = substr($strum, 0, -4);
+}
+
+
+if(isset($_POST['generi'])) {
+    $generi = $_POST['generi'];
+    $gen = 'OR ';
+    foreach($generi as $genere) {
+        $gen .= " '".pg_escape_string($dbconn, $genere )."' = any(generi_musicali) OR ";
+    }
+    $gen = substr($gen, 0, -4);
+}
+
+if(isset($_POST['servizi'])) {
+    $servizi = $_POST['servizi'];
+    $serv = 'OR ';
+    foreach($servizi as $servizio) {
+        $serv .= " '".pg_escape_string($dbconn, $servizio )."' = any(servizi_forniti) OR ";
+    }
+    $serv = substr($serv, 0, -4);
+}
 
 
 $start = ($page - 1) * $limit;
@@ -28,10 +70,30 @@ foreach($q as $text) {
 }
 $condition = substr($condition, 0, -4);
 
-$query = "SELECT * FROM v_profilo_artista WHERE ". $condition ." ORDER BY id_artista LIMIT $limit OFFSET $start" ;
-$query_2 = "SELECT COUNT(id_artista) FROM v_profilo_artista WHERE ". $condition;
+if(isset($_POST['citta']) AND $_POST['citta'] != NULL) {
+    $citta = $_POST['citta'];
+    $condition .= " AND id_citta = '".pg_escape_string($dbconn, $citta )."'";
+}
+    
+
+if(isset($_POST['strumenti'])){
+    $query = "SELECT * FROM v_profilo_artista WHERE ". $condition ." AND (".$strum." ".$gen." ".$serv.") ORDER BY ".$ordine." LIMIT $limit OFFSET $start" ;
+    $query_2 = "SELECT COUNT(id_artista) FROM v_profilo_artista WHERE ". $condition ." AND (".$strum." ".$gen." ".$serv.")";
+}else if (isset($_POST['generi'])){  
+    $gen = substr($gen, 3);
+    $query = "SELECT * FROM v_profilo_artista WHERE ". $condition ." AND (".$gen." ".$serv.") ORDER BY ".$ordine." LIMIT $limit OFFSET $start" ;
+    $query_2 = "SELECT COUNT(id_artista) FROM v_profilo_artista WHERE ". $condition ." AND (".$gen." ".$serv.")";
+}else if (isset($_POST['servizi'])){
+    $serv = substr($serv, 3);
+    $query = "SELECT * FROM v_profilo_artista WHERE ". $condition ." AND (".$serv.") ORDER BY ".$ordine." LIMIT $limit OFFSET $start" ;
+    $query_2 = "SELECT COUNT(id_artista) FROM v_profilo_artista WHERE ". $condition ." AND (".$serv.")";
+}else{
+    $query = "SELECT * FROM v_profilo_artista WHERE ". $condition ." ORDER BY ".$ordine." LIMIT $limit OFFSET $start" ;
+    $query_2 = "SELECT COUNT(id_artista) FROM v_profilo_artista WHERE ". $condition ."";
+}
 
 
+//echo $query;
 $count_artisti = pg_fetch_row(pg_query($dbconn, $query_2))[0];
 
 $result = pg_query($dbconn, $query) or die('Query failed: ' . pg_last_error());
@@ -40,13 +102,51 @@ $result = pg_query($dbconn, $query) or die('Query failed: ' . pg_last_error());
 if($count_artisti> 0) {
     while($row = pg_fetch_array($result)) {
         $display .= '<div class="item-list-fed">
-        <img id="foto_profilo" src='. $row['foto_profilo'] .' alt="foto profilo" class="flex-shrink-0 me-3" />
+        <img id="foto_profilo" src='. $row['foto_profilo'] .' alt="foto profilo"  class="flex-shrink-0 me-3" />
         <div class="col-md-4 p-2">
-          <h5 class="nome artista" >'. $row['nome'] .' </h5>
-          <p> '. $row['descrizione'] .' </p>
+          <h5 class="nome artista"  >'. $row['nome'] .' </h5>';
+            if($row['valutazione_media']){
+                $display .= '<h6 class="valutazione" style="color:#fd7e14"> 
+                 <i class="bi bi-star-fill"></i> <i class="bi bi-star"></i> 
+                 </h6>';
+            }else{
+                $display .= '<h6 class="valutazione" style="color:#fd7e14"> nessuna valutazione </h6>';
+            }
+        $display .= '
+          <p> '. substr($row['descrizione'],0,100) .' </p>
+          <a href="profilo_artista.php?id='. $row['id_artista'] .'" class="btn btn-primary"> Vedi profilo </a>
+        </div>
+        <div class="col-md-3 p-4">
+            <h5 style="color:#fd7e14; margin-top:5%; text-align:end"> '. $row['min_prezzo'] .' - '. $row['max_prezzo'] .' € </h5>';
+
+          $s = explode(",",substr($row['strumenti_musicali'],1,-1));
+          foreach($s as $str) {
+            if ($str != "")
+            $display .= '<span class="badge bg-secondary"> '. $str .' </span>';
+          }
+
+          $s = explode(",",substr($row['generi_musicali'],1,-1));
+            foreach($s as $str) {
+                if ($str != "")
+                $display .= '<span class="badge bg-info"> '. $str .' </span>';
+          }
+
+            $s = explode(",",substr($row['servizi_forniti'],1,-1));
+            foreach($s as $str) {
+                if ($str != ""){
+                    if($str[0] == '"' && $str[-1] =='"')
+                    $str = substr($str,1,-1);
+                    $display .= '<span class="badge bg-primary"> '. $str .' </span>';
+                }
+            }
+
+
+        $display .= '
         </div>
       </div>';
     }
+} else {
+    $display .= '<h3 style="text-align:center;"> Nessun artista trovato </h3>';
 }
 
 $total_pages = ceil($count_artisti / $limit);
@@ -56,7 +156,7 @@ $display .= '    <nav aria-label="Page navigation">
                     <ol class="pagination justify-content-end">';
     if($page > 1) {
         $previous = $page - 1;  
-        $display .= '<li class="page-item" id="1" ><a class="page-link" > 1 </a></li>';    
+        $display .= '<li class="page-item" id="1" ><a class="page-link" > << </a></li>';    
         $display .='  <li class="page-item" id="'.$previous.'" ><a class="page-link" > < </a></li>';
     }
    for($i=1; $i<= $total_pages; $i++) {
@@ -71,7 +171,7 @@ $display .= '    <nav aria-label="Page navigation">
    if($page < $total_pages) {
         $page++;
         $display .= '<li class="page-item"  id="'.$page.'"><a class="page-link" > > </a></li>';
-        $display .= '<li class="page-item" id="'.$total_pages.'" ><a class="page-link" > '.$total_pages.' </a></li>';
+        $display .= '<li class="page-item" id="'.$total_pages.'" ><a class="page-link" > >> </a></li>';
    }
 
    $display .='</ol> </nav>';
