e6be3c3294e476ac3675c63faa37d3e365d06a20
Arianna Paolini <ariannapaolini011@gmail.com>
2023-05-11 09:34:13 +0200

    11/05 registrazione

---
+++ pages/login/formValidation.js
@@ -0,0 +1,43 @@
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!checkValidity(form)) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+
+  function checkValidity(form)
+  {
+    var nome = form.inputName.value;
+    return  nome != "";
+  }
+
+
+
+
+// function validaForm()
+// {
+//     var form = document.getElementById("registration");
+//     window.alert("validazione");
+//     if (checkDataNascita()) {window.alert("data non valida"); form.classList.add("was-validated");}
+//     return checkDataNascita();
+// }
+
+// function checkDataNascita()
+// {
+//     var dataNascita = document.registration.inputBirthdate.value;
+//     var dataCorrente = new Date();
+//     if (dataNascita > dataCorrente) return false;
+//     return true;
+// }
--- pages/login/registration.php
+++ pages/login/registration.php
@@ -12,6 +12,7 @@
   <link rel="stylesheet" href="/bootstrap-icons-1.10.4/font/bootstrap-icons.css">
   <link rel="stylesheet" href="/pages/common/style.css" />
   <script src="/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="formValidation.js"></script> 
 
 </head>
 
@@ -29,57 +30,79 @@
 
     <br>
 
-    <form class="row g-3 px-3" method="POST">
+    <form id="registration" class="row g-3 px-3 needs-validation" method="POST" onSubmit="return validaForm()" novalidate>
         <h4 class="text-orange">Dati personali </h4>
         <div class="col-md-6">
             <label for="inputName" class="form-label">Nome</label>
-            <input type="text" class="form-control" id="inputName" name="nome" maxlength="20" placeholder="Inserisci il tuo nome">
+            <input type="text" class="form-control" id="inputName" name="nome" maxlength="20" placeholder="Inserisci il tuo nome" required>
+            <div class="invalid-feedback">Campo obbligatorio </div>
         </div>
         <div class="col-md-6">
             <label for="inputSurname" class="form-label">Cognome</label>
-            <input type="text" class="form-control" id="inputSurname" name="cognome" maxlength="20" placeholder="Inserisci il tuo cognome">
+            <input type="text" class="form-control" id="inputSurname" name="cognome" maxlength="20" placeholder="Inserisci il tuo cognome" required>
+            <div class="invalid-feedback">Campo obbligatorio </div>
         </div>
         <div class="col-md-6">
-            <label for="inputBirthdate" class="form-label">Data di nascita</label>
+            <label for="inputBirthdate" class="form-label">Data di nascita <i class="text-grey"> Opzionale </i> </label>
             <input type="date" class="form-control" id="inputBirthdate" name="datanascita">
+            <div class="invalid-feedback">Data di nascita non valida</div>
         </div>
         <div class="col-md-6">
-            <label for="inputCity" class="form-label">Città di residenza</label>
+            <label for="inputCity" class="form-label">Città di residenza <i class="text-grey"> Opzionale </i> </label>
             <select class="form-select" name="citta" id="inputCity">
                 <option selected>Seleziona la tua città</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
+                <?php
+                  include "../../connection.php";
+                  $query = "SELECT * FROM citta Order by nome_citta ASC";
+                  $result = pg_query($dbconn, $query) or die('Query failed: ' . pg_last_error());
+                  $citta = pg_fetch_all($result);
+                  foreach ($citta as $c) {
+                  echo '<option value = "', $c['id_citta'], '">', $c['nome_citta'], '</option>';
+                  } ?>
         </select>
         </div>
         <h4 class="text-orange mt-4">Dati account </h4>
        
-        <div class="col-12"> 
+        <div class="col-md-6"> 
             <label for="inputNick" class="form-label">Nickname</label>
             <div class="input-group">
             <span class="input-group-text">@</span>
-            <input type="text" class="form-control" id="inputNick" name="nickname" placeholder="Nickname" maxlength="15">
+            <input type="text" class="form-control" id="inputNick" name="nickname" placeholder="Nickname" maxlength="15" required>
             </div>
             <div class="form-text">Scegli un nickname non ancora in uso</div>
+            <div class="invalid-feedback">Nickname già in uso</div>
+        </div>
+        <div class="col-md-6">
+          <label for="inputPicture" class="form-label">Foto Profilo <i class="text-grey"> Opzionale </i></label>
+          <div class="input-group">
+          <label class="input-group-text" for="inputPicture"><i class="bi bi-file-earmark-person"></i></label>
+          <input type="file" name="picture" class="form-control" id="inputPicture">
+          </div>
         </div>
         <div class="col-md-6">
             
             <label for="inputEmail" class="form-label">Email</label>
-            <input type="email" class="form-control" name="email" id="inputEmail" maxlength="64" placeholder="Inserisci la tua email">
+            <input type="email" class="form-control" name="email" id="inputEmail" maxlength="64" placeholder="Inserisci la tua email" required>
+            <div class="invalid-feedback">Email non valida</div>
         </div>
         <div class="col-md-6">
             <label for="inputPassword" class="form-label">Password</label>
-            <input type="password" class="form-control" name="password" id="inputPassword" minlength="8" maxlength="15" placeholder="Crea una password">
+            <input type="password" class="form-control" name="password" id="inputPassword" minlength="8" maxlength="15" placeholder="Crea una password"  required>
             <div class="form-text">La password deve contenere almeno 8 caratteri</div>
+            <div class="invalid-feedback">Campo obbligatorio </div>      
         </div>
-    
-        foto profilo
-        bio
+        <div class="col-12">
+            <label for="inputBio" class="form-label">Bio  <i class="text-grey"> Opzionale </i> </label>
+            <textarea class="form-control" name="bio" id="inputBio" maxlength="1024" placeholder="Scrivi qualcosa su di te..."></textarea>
+        </div>
+
   
-  <div class="col-12">
+  <div class="col-12 text-center">
     <button type="submit" class="btn btn-primary">Invia</button>
   </div>
 </form>
+
+              
  
   
   <br>
